0d484520a30c20d1aba1454466db864b223de656
mikub <miroslav.kubicek@gmail.com>
2026-08-03 10:11:20 +0200

    Package samuraipersistor as an uberjar

--- build.clj
+++ build.clj
@@ -2,10 +2,30 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def class-dir "target/classes")
+(def uber-file "target/samuraipersistor.jar")
 (def basis (b/create-basis {:project "deps.edn"}))
+
+(defn clean [_]
+  (b/delete {:path "target"})
+  nil)
 
 (defn compile-java [_]
   (b/javac {:src-dirs  ["src/java"]
             :class-dir class-dir
             :basis     basis})
+  nil)
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs   ["resources"]
+               :target-dir class-dir})
+  (compile-java nil)
+  (b/compile-clj {:basis     basis
+                  :src-dirs  ["src/clj"]
+                  :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis     basis
+           :main      'samuraipersistor.core
+           :exclude   ["(?i)^META-INF/license(/.*)?$"]})
   nil)
